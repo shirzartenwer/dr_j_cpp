e9d193782db5ec3712606f1205ef2ad9e20fdc72
Shirzart Enwer <shirzartenwer@gmail.com>
2025-02-13 22:44:04 +0100

    feat: first few basic things

---
+++ TextFun/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+    char singelChar = 'a';
+    string myName = "lion";
+
+    cout << myName << endl;
+    cout << singelChar << endl;
+
+    return 0; 
+
+}
---
+++ test_1_vscode/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+// cout ocomes from the iostream
+using namespace std;
+// because of using std, there is no need to write std::cout etc, but cout is enough
+
+int main () {
+    
+    cout<<"Hello Shizzy"<<endl<<"John"<<endl<<"Mesut"<<endl;
+
+    return 0;
+}
