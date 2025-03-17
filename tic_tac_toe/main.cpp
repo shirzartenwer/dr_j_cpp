56991543df15baaa6f29e3f8c4aa241970f443f1
Shirzart Enwer <shirzartenwer@gmail.com>
2025-03-17 14:03:32 +0100

    feat: finished the tic-tac-toe game

--- tic_tac_toe/main.cpp
+++ tic_tac_toe/main.cpp
@@ -11,7 +11,9 @@ void runGame();
 void initializeGameBoard(array<array<string, 3>, 3>& gameBoard); // Pass by reference
 void printCurrentBoard(const array<array<string, 3>, 3>& gameBoard); // Pass by const reference
 void getUserInput(bool xTurn, array<array<string, 3>, 3>& gameBoard);
-tuple<string, int, int>getAndVerifyUserInput(bool xTurn, array<array<string, 3>, 3>& gameBoard);
+tuple<string, int, int> getAndVerifyUserInput(bool xTurn, array<array<string, 3>, 3>& gameBoard);
+void checkWinner(string& Winner, array<array<string, 3>, 3>& gameBoard);
+bool boardFull(array<array<string, 3>, 3>& gameBoard);
 
 int main(){
    
@@ -30,20 +32,22 @@ void runGame(){
         {" ", " ", " "},
         {" ", " ", " "}
     }};
+    initializeGameBoard(gameBoard);
+     // show the current game board
+    printCurrentBoard(gameBoard);
     while (Winner==""){
-        initializeGameBoard(gameBoard);
-        // show the current game board
-        printCurrentBoard(gameBoard);
         // take user input, verify and place a piece
         getUserInput(xTurn, gameBoard);
-    
         // check if there is winner 
+        checkWinner(Winner, gameBoard);
         // print hte current board
         printCurrentBoard(gameBoard);
+        xTurn = !xTurn;
     }
 
-        
+    cout <<"The game is finished, the winner is: " << Winner << endl;
 
+    
 }
 
 void initializeGameBoard(array<array<string, 3>, 3>& gameBoard){
@@ -83,7 +87,7 @@ tuple<string, int, int>getAndVerifyUserInput(bool xTurn, array<array<string, 3>,
 
 
     // validate if this space is empty for placing the piece 
-    if (gameBoard[tempRow][tempCol] != EMTPY_SPOT){
+    if (gameBoard[tempRow-1][tempCol-1] != EMTPY_SPOT){
         cout << "The locaiton you want to put the piece is not empty. Please input another one." << endl;
         return getAndVerifyUserInput(xTurn, gameBoard);
     }
@@ -96,4 +100,45 @@ tuple<string, int, int>getAndVerifyUserInput(bool xTurn, array<array<string, 3>,
         return pieceAndLoc;
     }
 
+}
+
+void checkWinner(string& Winner, array<array<string, 3>, 3>& gameBoard){
+    if(gameBoard[0][0] == gameBoard[0][1] && gameBoard[0][1] == gameBoard[0][2] && gameBoard[0][0]!=EMTPY_SPOT){
+        Winner = gameBoard[0][0];
+    } else if (gameBoard[1][0] == gameBoard[1][1] && gameBoard[1][1] == gameBoard[1][2] && gameBoard[1][0]!=EMTPY_SPOT)
+    {
+        Winner = gameBoard[1][0];
+    } else if (gameBoard[2][0] == gameBoard[2][1] && gameBoard[2][1] == gameBoard[2][2] && gameBoard[2][0]!=EMTPY_SPOT){
+        Winner = gameBoard[2][0];
+
+    } else if (gameBoard[0][0] == gameBoard[1][0] && gameBoard[1][0] == gameBoard[2][0] && gameBoard[0][0]!=EMTPY_SPOT){
+        Winner = gameBoard[0][0];
+    } else if (gameBoard[0][1] == gameBoard[1][1] && gameBoard[1][1] == gameBoard[2][1] && gameBoard[0][1]!=EMTPY_SPOT)
+    {
+        Winner = gameBoard[0][1];
+    } else if (gameBoard[0][2] == gameBoard[1][2] && gameBoard[2][2] == gameBoard[1][2] && gameBoard[0][2]!=EMTPY_SPOT)
+    {
+        Winner = gameBoard[0][2];
+    } else if (gameBoard[0][0] == gameBoard[1][1] && gameBoard[1][1] == gameBoard[2][2] && gameBoard[0][0]!=EMTPY_SPOT)
+    {
+        Winner = gameBoard[2][2];
+    } else if (gameBoard[0][2] == gameBoard[1][1] && gameBoard[1][1] == gameBoard[2][0] && gameBoard[0][2]!=EMTPY_SPOT)
+    {
+        Winner = gameBoard[2][2];
+    } else {
+        if (boardFull(gameBoard)){
+            Winner = "C";
+        }
+    }   
+}
+
+bool boardFull(array<array<string, 3>, 3>& gameBoard){
+    for (int i = 0; i < ROWS; i++){
+        for (int j = 0; j < COLS; j++){
+            if (gameBoard[i][j] == EMTPY_SPOT){
+                return false;
+            }
+        }
+    }
+    return true;
 }
